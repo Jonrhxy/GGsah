7a8f7dfd2c9576a218a05ed4bfe2117c3f11885a
John Garcia <garcia.john.erick.06082002@gmail.com>
2025-01-29 18:36:18 +0800

    gg sah

---
+++ app/src/main/java/com/example/testgame/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.testgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void startGame(View view) {
+
+
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
